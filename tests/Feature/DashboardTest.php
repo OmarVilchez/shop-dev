9aea8df5b01b7855be673c52f78513e684777bb9
sk-omarvilchez <cvilchez@sinsatu.com>
2025-07-04 23:02:46 -0500

    ultimo cambio en test n6

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -2,10 +2,16 @@
 
 use App\Models\User;
 
-test('guests are redirected to the login page', function () {
+// test('guests are redirected to the login page', function () {
+//     $response = $this->get('/');
+//     $response->assertRedirect('/login');
+// });
+
+test('guests can see the home page', function () {
     $response = $this->get('/');
-    $response->assertRedirect('/login');
+    $response->assertStatus(200);
 });
+
 
 test('authenticated users can visit the home', function () {
     $user = User::factory()->create();
